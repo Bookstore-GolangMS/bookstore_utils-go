06690bd012ead3a4f6e749e2612a5b5101bacceb
HunnTeRUS <otavio201378@gmail.com>
2021-08-28 23:30:17 -0300

    Changing logger library

--- logger/logger.go
+++ logger/logger.go
@@ -1,13 +1,18 @@
 package logger
 
 import (
+	"fmt"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
 var (
-	log *zap.Logger
+	log logger
 )
+
+type logger struct {
+	log *zap.Logger
+}
 
 func init() {
 	logConfig := zap.Config{
@@ -26,22 +31,30 @@ func init() {
 	}
 
 	var err error
-	if log, err = logConfig.Build(); err != nil {
+	if log.log, err = logConfig.Build(); err != nil {
 		panic(err)
 	}
 }
 
-func GetLogger() *zap.Logger {
+func GetLogger() logger {
 	return log
 }
 
+func (l logger) Printf(format string, v ...interface{}) {
+	if len(v) == 0 {
+		Info(format)
+	} else {
+		Info(fmt.Sprintf(format, v...))
+	}
+}
+
 func Info(message string, tags ...zap.Field) {
-	log.Info(message, tags...)
-	log.Sync()
+	log.log.Info(message, tags...)
+	log.log.Sync()
 }
 
 func Error(message string, err error, tags ...zap.Field) {
 	tags = append(tags, zap.NamedError("error", err))
-	log.Error(message, tags...)
-	log.Sync()
+	log.log.Error(message, tags...)
+	log.log.Sync()
 }
